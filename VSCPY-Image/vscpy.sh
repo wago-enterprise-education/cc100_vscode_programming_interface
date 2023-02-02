b213c0cb8b7e44c27840824c416020362e502bdd
Konrad Holsmölle <Konrad.Holsmoelle@wago.com>
2023-02-02 11:51:00 +0100

    Update vscpy.sh

--- VSCPY-Image/vscpy.sh
+++ VSCPY-Image/vscpy.sh
@@ -4,4 +4,4 @@ docker run -d --name=code-server -e PUID=1000 -e PGID=1000 -e TZ=Europe/Berlin -
 -v /sys/kernel/dout_drv:/home/ea/dout \
 -v /sys/devices/platform/soc/44009000.spi/spi_master/spi0/spi0.0:/home/ea/din \
 -v /sys/devices/platform/soc/40017000.dac:/home/ea/anout \
---privileged vscpy_v3
+--privileged konradholsmoelle/vscpy
