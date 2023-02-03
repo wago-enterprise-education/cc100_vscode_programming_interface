a7766ef1f10c85cc202064cc6224f1c90522551f
Konrad Holsmölle <Konrad.Holsmoelle@wago.com>
2023-02-03 12:02:45 +0100

    Ergänzen weitere Volumes
    Ergänzen des Volumes für die anlogen Eingänge
    Ergänzen des Volumes für die Kalibrierungsdatei
    Ändern des Names vom Image zum CC100 Image

--- VSCPY-Image/vscpy.sh
+++ VSCPY-Image/vscpy.sh
@@ -4,4 +4,6 @@ docker run -d --name=code-server -e PUID=1000 -e PGID=1000 -e TZ=Europe/Berlin -
 -v /sys/kernel/dout_drv:/home/ea/dout \
 -v /sys/devices/platform/soc/44009000.spi/spi_master/spi0/spi0.0:/home/ea/din \
 -v /sys/devices/platform/soc/40017000.dac:/home/ea/anout \
---privileged konradholsmoelle/vscpy
+-v /etc/calib:/home/ea/cal/calib \
+-v /sys/devices/platform/soc/48003000.adc:/home/ea/anin \
+--privileged konradholsmoelle/vscpy:cc100.1
