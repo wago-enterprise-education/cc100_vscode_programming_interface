815bfa48dfce0953b2506e731f6a9a2a10163077
Bjarne Zaremba <bjarne.zaremba@wago.com>
2023-02-13 09:38:11 +0100

    Mergeconflicts beseitigt

--- VSCPY-Image/VSCPY-CC100-Image/PythonModule/CC100IO.py
+++ VSCPY-Image/VSCPY-CC100-Image/PythonModule/CC100IO.py
@@ -150,9 +150,5 @@ def analogRead(iEingang: int):
     iSpannung=int(f.readline())
     f.close()
     # Calibrates the value and returns it
-<<<<<<< Updated upstream
-    return(Cal.calibrateIn(iSpannung, iEingang))
-=======
     return(Cal.calibrateIn(iSpannung, iEingang))
     
->>>>>>> Stashed changes
