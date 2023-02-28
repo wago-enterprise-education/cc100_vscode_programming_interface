19698e1d2f22228d33cc3d0b49b13e4b309b3f77
Bjarne Zaremba <bjarne.zaremba@wago.com>
2023-02-28 10:50:07 +0100

    Python Modul auf anders Repo umgestellt

--- VSCPY-Image/funktionierendeVersion.md
+++
@@ -1,2 +0,0 @@
-linuxserver/code-server:4.9.1<br>
-python3.10.6
--- VSCPY-Image/vscpy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker run -d --name=code-server -e PUID=1000 -e PGID=1000 -e TZ=Europe/Berlin -p 8443:8443 -v /home/code-server/config:/config --restart unless-stopped --privileged \
--v /sys/kernel/dout_drv:/home/ea/dout \
--v /sys/devices/platform/soc/44009000.spi/spi_master/spi0/spi0.0:/home/ea/din \
--v /sys/devices/platform/soc/40017000.dac:/home/ea/anout \
--v /etc/calib:/home/ea/cal/calib \
--v /sys/devices/platform/soc/48003000.adc:/home/ea/anin \
---privileged konradholsmoelle/vscpy:cc100.1
