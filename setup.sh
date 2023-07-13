61c48b718b02d6799f1f52b931b13b621e6aae62
KonHol <124201065+KonHol@users.noreply.github.com>
2023-07-13 08:25:35 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -28,9 +28,8 @@ echo '{
 
 
 #
-# Download des Code-Server Image 
+# Modification of folders
 #
-docker pull wagoeducation/cc100vscode
 chmod -R 777 /sys/kernel/dout_drv/DOUT_DATA
 chmod -R 777 /sys/devices/platform/soc/44009000.spi/spi_master/spi0/spi0.0
 chmod -R 777 /sys/devices/platform/soc/40017000.dac
@@ -41,6 +40,7 @@ chmod -R 777 /sys/bus/iio/devices
 #
 #Start des Code-Server-Containers
 #
+docker pull wagoeducation/cc100vscode
 docker run -d --name=code-server -e PUID=1000 -e PGID=1000 -e TZ=Europe/Berlin -p 8443:8443 -p 3000:3000 -v /home/code-server/config:/config --restart unless-stopped \
 -v /sys/kernel/dout_drv:/home/ea/dout \
 -v /sys/devices/platform/soc/44009000.spi/spi_master/spi0/spi0.0:/home/ea/din \
